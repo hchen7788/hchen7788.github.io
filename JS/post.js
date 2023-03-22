8dc316277fbcd011a2f00950725da72efd44fa2b
HaitongC <haitongchen116@gmail.com>
2023-03-22 15:25:31 -0700

    click outside to exit

--- JS/post.js
+++ JS/post.js
@@ -1,11 +1,14 @@
 function expandPost(){
     const dia = document.getElementById("md-html");
+    const d = document.getElementById("md-content");
     dia.showModal();
+    dia.addEventListener('click', () => dia.close());
+    d.addEventListener('click', (event) => event.stopPropagation());
     const xhr = new XMLHttpRequest();
     xhr.open('GET', '../blogs-posts/2023-02-06.md', true);
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4 && xhr.status === 200) {
-            let output = document.getElementById('md-html');
+            let output = document.getElementById('md-content');
             output.innerHTML = xhr.responseText;
             // console.log(xhr.responseText); // log the file contents to the console
 
