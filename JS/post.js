b8645627119f0b95dbb1166bd079cfd38bb6bd5d
HaitongC <haitongchen116@gmail.com>
2023-03-21 23:29:18 -0700

    md to html draft finish

--- JS/post.js
+++ JS/post.js
@@ -1,9 +1,12 @@
-function searchPost(){
-    let dia = document.getElementById('searchDialog')
-    dia.showModal()
-
-    let input = document.getElementById('search-post').value;
-    document.getElementById('searchInput').innerText = input;
-
-    document.getElementById('search-post').value = "";
+function expandPost(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '../blogs-posts/2023-02-06.md', true);
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let output = document.getElementById('md-html');
+            output.innerHTML = xhr.responseText;
+            console.log(xhr.responseText); // log the file contents to the console
+          }
+    };
+    xhr.send();
 }
