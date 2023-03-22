5b8bb9204a0bd922500df288e5b1f43d4481d55f
HaitongC <haitongchen116@gmail.com>
2023-03-22 15:37:15 -0700

    file name as parameter so can be searched for

--- JS/post.js
+++ JS/post.js
@@ -1,11 +1,11 @@
-function expandPost(){
+function expandPost(fileName){
     const dia = document.getElementById("md-html");
     const d = document.getElementById("md-content");
     dia.showModal();
     dia.addEventListener('click', () => dia.close());
     d.addEventListener('click', (event) => event.stopPropagation());
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', '../blogs-posts/2023-02-06.md', true);
+    xhr.open('GET', `../blogs-posts/${fileName}`, true);
     xhr.onreadystatechange = function(){
         if (xhr.readyState === 4 && xhr.status === 200) {
             let output = document.getElementById('md-content');
