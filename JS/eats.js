c6b43067f2cd9f347b86a10db4f7d373150b2d7e
HaitongC <haitongchen116@gmail.com>
2023-03-24 00:57:18 -0700

    png to sub png function of eat posts done

--- JS/eats.js
+++ JS/eats.js
@@ -7,3 +7,13 @@ function leaveGif(fileName){
     let image = document.getElementById(`${fileName}Img`);
     image.setAttribute('src', `../eats-images/${fileName}.png`);
 }
+
+function toSub(fileName){
+    let image = document.getElementById(`${fileName}Img`);
+    image.setAttribute('src', `../eats-images/${fileName}Sub.png`);
+}
+
+function leaveSub(fileName){
+    let image = document.getElementById(`${fileName}Img`);
+    image.setAttribute('src', `../eats-images/${fileName}.png`);
+}
