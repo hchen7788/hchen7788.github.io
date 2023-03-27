c6b7f08ac9f155ebf64b7501e4c8b9a0830b0bc9
HaitongC <haitongchen116@gmail.com>
2023-03-27 07:15:46 -0700

    added log statement to see speed

--- JS/eats.js
+++ JS/eats.js
@@ -1,19 +1,27 @@
 function toGif(fileName){
+    console.log("in toGif");
     let image = document.getElementById(`${fileName}Img`);
     image.setAttribute('src', `../eats-images/${fileName}.gif`);
+    console.log("toGif done");
 }
 
 function leaveGif(fileName){
+    console.log("in leaveGif");
     let image = document.getElementById(`${fileName}Img`);
     image.setAttribute('src', `../eats-images/${fileName}.png`);
+    console.log("leaveGif done");
 }
 
 function toSub(fileName){
+    console.log("in toSub");
     let image = document.getElementById(`${fileName}Img`);
     image.setAttribute('src', `../eats-images/${fileName}Sub.png`);
+    console.log("toSub done");
 }
 
 function leaveSub(fileName){
+    console.log("in leaveSub");
     let image = document.getElementById(`${fileName}Img`);
     image.setAttribute('src', `../eats-images/${fileName}.png`);
+    console.log("leaveSub done");
 }
