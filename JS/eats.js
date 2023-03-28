04f5e57736eee0459cb0de0223985c6abfa80dd3
HaitongC <haitongchen116@gmail.com>
2023-03-27 21:55:08 -0700

    misc try

--- JS/eats.js
+++ JS/eats.js
@@ -1,8 +1,10 @@
 function toGif(fileName){
     console.log("in toGif");
+    alert("in toGif");
     let image = document.getElementById(`${fileName}Img`);
     image.setAttribute('src', `../eats-images/${fileName}.gif`);
     console.log("toGif done");
+    alert("leave toGif");
 }
 
 function leaveGif(fileName){
